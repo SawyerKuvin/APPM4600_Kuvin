568601948f984153bcf983c2de83c45475dafb04
SawyerKuvin <sawyer.kuvin@colorado.edu>
2025-01-24 20:08:55 -0700

    Final Homework code and figures

--- Homeworks/HW1/Homework1.py
+++ Homeworks/HW1/Homework1.py
@@ -24,7 +24,8 @@ plt.title("p(x) Plotted in Original Binomial Form")
 plt.show()
 
 # PROBLEM 2
-x = np.linspace(1.99999999,2.00000001,1000000)
+#Failed attempt to manually determine the error
+'''x = np.linspace(1.99999999,2.00000001,1000000)
 y = np.linspace(0.5,2.5,1000000)
 diff = np.abs(x-y)
 z1_bad = np.sin(x) - np.sin(y)
@@ -33,7 +34,7 @@ plt.figure(3)
 plt.plot(diff,z1_bad)
 plt.plot(diff,z1_good)
 plt.title("Error Determination Attempt - Failed")
-plt.show()
+plt.show()'''
 
 # PROBLEM 4
 # Using python  to find roots to a precision higher than 3 decimal places
@@ -71,30 +72,90 @@ fx2_a = np.cos(x2+del_vec)-np.cos(x2)
 fx1_b = -2 * np.sin(x1 + (del_vec / 2)) * np.sin(del_vec / 2)
 fx2_b = -2 * np.sin(x2 + (del_vec / 2)) * np.sin(del_vec / 2)
 
+
 # Plot comparing (a) and (b) for x1
 plt.figure(4)
 plt.grid(True)
-plt.semilogx(del_vec,fx1_a-fx1_b)
+plt.semilogx(del_vec,abs(fx1_a-fx1_b))
 plt.xlabel("Delta vector")
 plt.ylabel("Difference between models")
-plt.title("Difference comparison for cos(x1+delta) - cos(x1)")
+plt.title("Difference comparison for Original and Trig Model [3pi/4]")
 plt.show()
 
 # Plot comparing (a) and (b) for x2
 plt.figure(4)
 plt.grid(True)
-plt.semilogx(del_vec,fx2_a-fx2_b)
+plt.semilogx(del_vec,abs(fx2_a-fx2_b))
 plt.xlabel("Delta vector")
 plt.ylabel("Difference between models")
-plt.title("Difference comparison for cos(x2+delta) - cos(x2)")
+plt.title("Difference comparison for Original and Trig Model [10^7]")
 plt.show()
 
 
 
-# Iterationg most accurate ksi for part (c)
-#fx1_c = np.zeros((len(del_vec),100))
-#for z in range(len(del_vec)): 
-    #ksi = np.linspace(x1,x1+del_vec[z],100)
-    #fx1_c[z,:] = (- del_vec * np.sin(x1)) - (((del_vec ** 2) / 2) * np.cos(ksi))
+# Iterating most accurate ksi for part (c)
+# Test for x1, iterating through fractions of delta to determine a ksi value that
+# minimizes error 
+fx1_c = np.zeros(17)
+del_fracs = np.linspace(0,1,100000)
+print(del_fracs)
+for d in range(17):
+    ksi1 = x1+(del_vec[d]*del_fracs)
+    fx1_c_test = -(del_vec[d] * np.sin(x1)) - ((del_vec[d]**2/2) * np.cos(ksi1))
+    err = abs(fx1_b[d] - fx1_c_test)
+    #print(err)
+    min_arg1 = np.argmin(err)
+    #print(min_arg1)
+    fx1_c[d] = -(del_vec[d] * np.sin(x1)) - ((del_vec[d]**2/2) * np.cos(ksi1[min_arg1]))
 
-#print(fx1_c)
+print(abs(fx1_c-fx1_b))
+
+diffs1 = abs(fx1_c-fx1_b)
+plt.figure(5)
+plt.grid(True)
+plt.semilogx(del_vec,diffs1)
+plt.xlabel("Delta vector")
+plt.ylabel("Difference between models")
+plt.title("Difference comparison for Trig model and Algorithm [3pi/4]")
+plt.show()
+
+#Plot with first poor elements removed
+plt.figure(6)
+plt.grid(True)
+plt.semilogx(del_vec[:-3],diffs1[:-3])
+plt.xlabel("Delta vector")
+plt.ylabel("Difference between models")
+plt.title("Difference comparison for Trig model and Algorithm [3pi/4]")
+plt.show()
+
+fx2_c = np.zeros(17)
+del_fracs = np.linspace(0,1,100000)
+print(del_fracs)
+for d in range(17):
+    ksi2 = x2+(del_vec[d]*del_fracs)
+    fx2_c_test = -(del_vec[d] * np.sin(x2)) - ((del_vec[d]**2/2) * np.cos(ksi2))
+    err = abs(fx2_b[d] - fx2_c_test)
+    #print(err)
+    min_arg2 = np.argmin(err)
+    #print(min_arg1)
+    fx2_c[d] = -(del_vec[d] * np.sin(x2)) - ((del_vec[d]**2/2) * np.cos(ksi2[min_arg2]))
+
+print(abs(fx2_c-fx2_b))
+
+diffs2 = abs(fx2_c-fx2_b)
+plt.figure(7)
+plt.grid(True)
+plt.semilogx(del_vec,diffs2)
+plt.xlabel("Delta vector")
+plt.ylabel("Difference between models")
+plt.title("Difference comparison for Trig model and Algorithm [10^7]")
+plt.show()
+
+#Plot with first poor elements removed
+plt.figure(8)
+plt.grid(True)
+plt.semilogx(del_vec[:-2],diffs2[:-2])
+plt.xlabel("Delta vector")
+plt.ylabel("Difference between models")
+plt.title("Difference comparison for Trig model and Algorithm [10^7]")
+plt.show()
